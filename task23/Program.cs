c9d2983d3570471370cf9265bfe13d10af7f7669
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-09 22:58:01 +0800

    Решение задачи 23

--- task23/Program.cs
+++ task23/Program.cs
@@ -4,3 +4,17 @@
 
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i*i} ");
+}
+
+//Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
